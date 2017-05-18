5cc5ee39dbfe4acf39ea699329d00538aa9680ad
sharmav <mollievx@gmail.com>
2017-05-18 18:20:05 +0200

    Modified init and transfer for FITcoin

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -19,7 +19,8 @@ package main
 import (
 	"errors"
 	"fmt"
-
+	"encoding/json"
+	"strconv"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -34,13 +35,55 @@ func main() {
 	}
 }
 
+//structure for account
+type Account struct {
+	Name     string `json:"name"`
+	Balance  int    `json:"balance"`
+}
+
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	if len(args) != 0 {
+		return nil, errors.New("No arguments required")
 	}
 
-	err := stub.PutState("Vatsala", []byte(args[0]))
+	var account1, account2, account3 Account
+	account1.Name = "Vatsala"
+	account1.Balance = 1000
+	account2.Name = "Harish"
+	account2.Balance = 1000
+	account3.Name = "Narayan"
+	account3.Balance = 1000
+
+	b, err := json.Marshal(account1)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for account 1")
+	}
+
+	err = stub.PutState("Vatsala", b)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err = json.Marshal(account2)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for account 2")
+	}
+
+	err = stub.PutState("Harish", b)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err = json.Marshal(account3)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for account 3")
+	}
+
+	err = stub.PutState("Narayan", b)
 	if err != nil {
 		return nil, err
 	}
@@ -57,6 +100,8 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "transfer"{
+		return t.transfer(stub, args)
 	}
 	fmt.Println("invoke did not find func: " + function)
 
@@ -112,4 +157,74 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	}
 
 	return valAsbytes, nil
+}
+
+//Function to transfer funds from one account to another
+func (t *SimpleChaincode) transfer(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var jsonResp string
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	var accFrom Account
+	var accTo Account
+	var amount int
+
+	accFromAsBytes, err := stub.GetState(args[0])
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + args[0] + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	err = json.Unmarshal(accFromAsBytes, &accFrom)
+	if err != nil {
+		return nil, errors.New("Failed to marshal string to struct of accFrom")
+	}
+
+	accToAsBytes, err := stub.GetState(args[1])
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + args[1] + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	err = json.Unmarshal(accToAsBytes, &accTo)
+	if err != nil {
+		return nil, errors.New("Failed to marshal string to struct of accTo")
+	}
+
+	amount, err = strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Enter an integer value in the 'Amount'")
+	}
+
+	if accFrom.Balance < amount {
+		return nil, errors.New("Insufficient Balance")
+	}
+
+	accFrom.Balance = accFrom.Balance - amount
+	accTo.Balance = accTo.Balance + amount
+
+	b, err := json.Marshal(accFrom)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for account 1")
+	}
+
+	err = stub.PutState(args[0], b)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err = json.Marshal(accTo)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for account 1")
+	}
+
+	err = stub.PutState(args[1], b)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+
 }
